7098c52287f99a96dc3de5e9655af199cdbc0be2
Ravi Kumar Singh <rkspsm@gmail.com>
2017-12-09 21:17:06 +0530

    removed implementations from formal spec

--- src/formal/model.v
+++ src/formal/model.v
@@ -12,33 +12,9 @@ Inductive location :=
 | loc_door
 .
 
-Definition loc_row := list location.
-Definition loc_map := list loc_row.
+Definition loc_map := list (list location).
 
-Definition beq_loc (l1 l2 : location) : bool :=
-  match l1,l2 with
-  | loc_empty, loc_empty => true
-  | loc_player, loc_player => true
-  | loc_wall, loc_wall => true
-  | loc_key, loc_key => true
-  | loc_door, loc_door => true
-  | _,_ => false
-  end.
-
-Fixpoint loc_row_count (lr : loc_row) (l : location) : nat :=
-  match lr with
-  | nil => O
-  | l' :: lr' => match (beq_loc l l') with
-                 | true => S (loc_row_count lr' l)
-                 | false => loc_row_count lr' l
-                 end
-  end.
-
-Fixpoint loc_map_count (lm : loc_map) (l : location) : nat :=
-  match lm with
-  | nil => 0
-  | lr' :: lm' => (loc_row_count lr' l) + (loc_map_count lm' l)
-  end.
+Parameter loc_map_count : loc_map -> location -> nat.
 
 Definition loc_map_valid (lm : loc_map) : Prop :=
   (forall l1 l2, In l1 lm -> In l2 lm -> length l1 = length l2) /\
