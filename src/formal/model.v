5419bc2f96599ae5fdd919d543f1ab32f91b00aa
Ravi Kumar Singh <rkspsm@gmail.com>
2017-12-09 21:11:44 +0530

    added process

--- src/formal/model.v
+++ src/formal/model.v
@@ -50,22 +50,28 @@ Inductive major_ui :=
 | ui_postgame
 | ui_menu
 | ui_designer
+| ui_replay
 .
-
-Inductive app_state :=
-| appst_launched
-| appst_running : major_ui -> app_state.
 
 Parameter map_index : Set.
 
-Definition is_visible := bool.
 Definition is_victorious := bool.
+Definition is_paused := bool.
+
+Inductive app_state :=
+| appst_launched
+| appst_game : is_paused -> app_state
+| appst_postgame : is_victorious -> app_state
+| appst_menu
+| appst_designer
+| appst_replay
+.
 
 Inductive timer_control :=
-| tc_activate
-| tc_deactivate
+| tc_reset
+| tc_run
+| tc_stop
 | tc_pause
-| tc_play
 .
 
 Definition coords := (prod nat nat).
@@ -79,11 +85,13 @@ Inductive movement :=
     
 Inductive command :=
 | cmd_set_state : app_state -> command
-| cmd_set_visibility : major_ui -> is_visible -> command
+| cmd_select_ui : major_ui -> command
 | cmd_set_postgame : is_victorious -> command
-| cmd_game_timer_activate : timer_control -> command
+| cmd_game_timer : timer_control -> command
 | cmd_game_pause : bool -> command
 | cmd_select_map : map_index -> command
+| cmd_game_fuel_tick
+| cmd_game_process_move
 .
 
 Inductive stimulus :=
@@ -96,6 +104,71 @@ Inductive stimulus :=
 | stls_game_tick
 | stls_game_pause : bool -> stimulus
 | stls_game_quit
+| stls_game_fuel_empty
 .
 
-Inductive process : app_state -> stimulus -> list command -> Prop :=.
+Inductive process : app_state -> stimulus -> list command -> Prop :=
+
+| proc_launched :
+    process appst_launched stls_launched
+            [ cmd_set_state appst_menu ;
+                cmd_select_ui ui_menu
+            ]
+
+| proc_menu_map_selected : forall mi,
+    process appst_menu (stls_menu_map_selected mi)
+            [ cmd_select_map mi ;
+                cmd_select_ui ui_game ;
+                cmd_game_timer tc_reset ;
+                cmd_game_timer tc_run ;
+                cmd_set_state (appst_game false)
+            ]
+
+| proc_game_tick :
+    process (appst_game false) stls_game_tick
+            [ cmd_game_fuel_tick ]
+
+| proc_game_paused :
+    process (appst_game false) (stls_game_pause true)
+            [ cmd_game_timer tc_pause ;
+                cmd_set_state (appst_game true)
+            ]
+
+| proc_game_resumed :
+    process (appst_game true) (stls_game_pause false)
+            [ cmd_game_timer tc_run ;
+                cmd_set_state (appst_game false)
+            ]
+
+| proc_game_move : forall m,
+    process (appst_game false) (stls_game_moved m)
+            [ cmd_game_process_move ]
+.
+
+Inductive move_command :=
+| mvcmd_update_coords : coords -> location -> move_command
+| mvcmd_update_doors
+| mvcmd_trigger_victory
+| mvcmd_expend_fuel
+| mvcmd_update_player_coords
+.
+
+Parameter move_within_bounds : forall (src : coords) (m : movement) (dest : coords), Prop.
+Parameter map_location : forall (m : loc_map) (c : coords) (l : location), Prop.
+
+Inductive process_move : loc_map -> coords -> movement -> list move_command -> Prop :=
+
+| pmv_to_empty : forall board src mv _dest,
+    move_within_bounds src mv _dest ->
+    map_location board _dest loc_empty ->
+    process_move board src mv
+                 [ mvcmd_expend_fuel ;
+                     mvcmd_update_coords src loc_empty ;
+                     mvcmd_update_coords _dest loc_player ;
+                     mvcmd_update_player_coords _dest
+                 ]
+
+| pmv_to_key : forall board src mv _dest,
+    move_within_bounds src mv _dest ->
+    map_location board _dest loc_key ->
+.
